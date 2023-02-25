7bb57701b06f3a9dfed1620e3d2470ea84a36bce
xiaohang <wetalk.vip@foxmail.com>
2023-02-25 14:20:28 +0800

    一个爬虫

--- main.php
+++ main.php
@@ -8,6 +8,11 @@ $date = trim(fgets(STDIN));
 // 为空则默认为当天
 $date = empty($date) ? date('Y-m-d') : $date;
 
+$dateTime = DateTime::createFromFormat('Y-m-d', $date);
+if ($dateTime !== false && $dateTime->format('Y-m-d') !== $date) {
+    exit('日期格式错误，请重新输入');
+}
+
 // 发起请求
 $http = new Http();
 $data = $http->send($date);
